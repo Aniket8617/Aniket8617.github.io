c87068ec323f781ee37716c0ff3358c38d5b360a
Aniket8617 <aniketasutkar078@gmail.com>
2025-07-02 13:52:32 +0530

    Update main.py

--- student_portfolio/main.py
+++ student_portfolio/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# This block ensures compatibility with Render's environment
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Render sets PORT dynamically
     app.run(host='0.0.0.0', port=port)
